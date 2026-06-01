5834f9e409d97a8513318a8d3e44983ee6e4a231
X-Studios-ops <som423428@gmail.com>
2026-06-01 16:26:14 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,15 @@
-self.addEventListener('install', (e) => {
- e.waitUntil(
-   caches.open('heydoctor-store').then((cache) => cache.addAll(['/', '/index.html', '/logo.png']))
- );
+const CACHE_NAME = "heydoctor-v1";
+
+self.addEventListener("install", () => {
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
+self.addEventListener("activate", (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
